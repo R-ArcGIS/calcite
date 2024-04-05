f5cbc735c00a8c3c262ca1c0b07e55a8b723e510
Josiah Parry <josiah.parry@gmail.com>
2024-04-05 15:28:31 -0400

    update testing files

---
+++ R/calcite-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+
+#' Calcite components
+#'
+#' All calcite comopnents
+#' @name components
+#' @export
+NULL
--- dev/test.R
+++ dev/test.R
@@ -1,4 +1,11 @@
 library(shiny)
+library(calcite)
+
+
+
+# Read data --------------------------------------------------------------
+library(arcgislayers)
+earthquakes <- arc_read("https://services9.arcgis.com/RHVPKKiFTONKtxq3/ArcGIS/rest/services/USGS_Seismic_Data_v1/FeatureServer/0")
 
 # Custom script and style ------------------------------------------------
 
@@ -39,7 +46,7 @@ ui <- div(
       heading = "Earthquake results",
       "heading-level" = 1,
       description = "Search by location to display results",
-      calcite_filter(placeholder = "Try searching Alaska"),
+      calcite_filter(id = "filter_id", placeholder = "Try searching Alaska", style = "l"),
       calcite_notice(
         id = "initial-note", icon = "information",
         open = NA,
@@ -55,9 +62,53 @@ ui <- div(
     )
   ),
   htmltools::includeScript("dev/helper.js"),
+  # In your Shiny app's UI
+  shinyjs::useShinyjs(),
+  shinyjs::extendShinyjs(
+    text = "
+    $(document).ready(function() {
+      var calciteFilter = document.querySelector('calcite-filter');
+      calciteFilter.addEventListener('calciteFilterChange', function(e) {
+        var itemsString = JSON.stringify(e.target.items);
+        Shiny.forgetLastInputValue('calciteFilterItems');
+        Shiny.onInputChange('calciteFilterItems', itemsString);
+      });
+    });
+  ", functions = c()
+  ),
+  tags$script(
+    HTML("
+      document.addEventListener('DOMContentLoaded', function() {
+        var calciteFilter = document.querySelector('calcite-filter');
+        calciteFilter.addEventListener('calciteFilterChange', function(e) {
+          console.log('calciteFilterChange event triggered');
+          var itemsString = JSON.stringify(e.target.items);
+          console.log('itemsString:', itemsString);
+        });
+      });
+    ")
+  )
 )
 
 server <- function(input, output) {
+  observeEvent(input$calciteFilterItems,
+    {
+      items <- RcppSimdJson::fparse(input$calciteFilterItems)
+      print(str(items))
+    },
+    ignoreNULL = TRUE
+  )
 }
 
 shiny::shinyApp(ui, server)
+
+
+# DO i need to do this somewhere in the package
+
+# calcite_dep <- htmlDependency(
+#   "calcitejs", "2.7.1",
+#   src = "https://js.arcgis.com/calcite-components/2.7.1/calcite.esm.js",
+#   script = list(
+#     type = "module"
+#   ),
+# )
